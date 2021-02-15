d69f0e9eee75ee4366ced1f9d47daaa92e3a1c86
dteichholtz <davet@teichholtz.com>
2021-02-15 10:11:58 -0500

    Added the contractor app.

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 def home(request):
     return HttpResponse('<h1>Blog Home</h1>')
 
-
-
 def about(request):
     return HttpResponse('<h1>Blog About</h1>')
+
+def register(request):
+    return HttpResponse('<h1>Blog Register</h1>')
+
+def profile(request):
+    return HttpResponse('<h1>Blog Profile</h1>')

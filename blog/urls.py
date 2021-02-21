083f236702ff57a66ee68f1ddd80c49321d899bf
dteichholtz <davet@teichholtz.com>
2021-02-20 23:01:34 -0500

    Added the user sign in form and the css to go with it.  crispy-forms!

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
     path('register', views.register, name='blog-register'),
+    path('frap/', views.about, name='frap-about'),
     path('profile', views.profile, name='blog-profile')
 ]
 

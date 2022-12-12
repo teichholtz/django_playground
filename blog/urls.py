02bbb067c6d3767cd08ec1c0d500fdc0fedc4863
David Teichholtz <davet@teichholtz.com>
2022-12-12 16:10:13 -0500

    getting login link to work.  Also moved register to the sidebar.

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
     path('register/', views.register, name='blog-register'),
-    path('frap/', views.about, name='frap-about'),
+    path('login/', views.login, name='blog-login'),
     path('profile', views.profile, name='blog-profile')
 ]
 
